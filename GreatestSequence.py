b7efabd27e1821cd1546f591aa01d66016b303b8
Sonu Swapan Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2022-01-08 19:48:06 +0530

    Add files via upload

--- GreatestSequence.py
+++ GreatestSequence.py
@@ -1,12 +1,27 @@
 # In the following 6 digit number:
 # 283910 -->91 is the greatest sequence of 2 consecutive digits.
-
 # In the following 10 digit number:
 # 1234567890 -->67890 is the greatest sequence of 5 consecutive digits.
 # Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. 
-# The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
+# The number will be passed in as a string of only digits. It should return a five digit integer. 
+# The number passed may be as large as 1000 digits.
 
 def solution(digits): 
     numlist = [int(digits[i:i+5]) for i in range(0,len(digits)-4)] 
     return max(numlist)
-print(solution('1234890'))
+
+
+# digits='1234567'     
+# print(len(digits)-4)
+# numlist=[]
+# for i in range(0,len(digits)-4):
+#     numlist.append(int(digits[i:i+5]))
+# print(numlist)
+# print(max(numlist))
+
+
+print(solution('1234567890'))
+print(solution('123456789'))
+print(solution('12345678'))
+print(solution('1234567'))
+print(solution('123456'))
