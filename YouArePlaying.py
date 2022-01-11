c157bdcfc870ed50bfb7a59a385b1b94773c5ce9
Sonu Swapan Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2022-01-11 22:55:19 +0530

    Add files via upload

---
+++ YouArePlaying.py
@@ -0,0 +1,15 @@
+# Create a function which answers the question "Are you playing banjo?".
+# If your name starts with the letter "R" or lower case "r", you are playing banjo!
+# The function takes a name as its only argument, and returns one of the following strings:
+# name + " plays banjo" 
+# name + " does not play banjo"
+
+def areYouPlayingBanjo(name):
+    if name[0].lower() == 'r':
+        return name + ' plays banjo'
+    else:
+        return name + ' does not play banjo'
+    
+#     return name + (' plays' if name[0].lower() == 'r' else ' does not play') + " banjo"
+
+print(areYouPlayingBanjo("Rita"))
